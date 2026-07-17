5741f829d5b27e50b6093851b953868e4972a7d5
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2026-07-17 19:43:44 +0100

    Add github pages build workflow

--- _website/astro.config.mjs
+++ _website/astro.config.mjs
@@ -11,5 +11,7 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [solidJs()]
+  integrations: [solidJs()],
+  site: "https://hatchibombotar.com",
+  base: "/ebimui"
 });
